c23e6e0fbcd4bcda27557b8b600b73311a1c3886
Aloisio Vieira <aloisio.vieira@gmail.com>
2020-02-16 18:02:00 -0300

    Code coverage with jacoco

---
+++ src/test/java/guru/springframework/JavaHelloWorldIT.java
@@ -0,0 +1,15 @@
+package guru.springframework;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JavaHelloWorldIT {
+
+	@Test
+    void myFauxIntegrationTest() {
+
+        System.out.println("My IT Ran");
+    }
+
+}
